0f5a675e4ce0750a2f04a88d5c2d9c2fb06a641e
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-21 19:39:41 -0300

    first commit

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,17 @@
+import { Container, Icon, Title } from "./styles";
+
+type Props = {
+  title: string;
+  
+}
+
+export function GroupCard({title}: Props){
+  return(
+    <Container>
+      <Icon/>
+        <Title>
+          {title}
+        </Title>
+    </Container>
+  );
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,19 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
+import { Container } from './styles';
+
+export  function Groups() {
+  return (
+    <Container>
+     <Header />
+     <Highlight
+      title='Turmas'
+      subtitle='Jogue com a sua turma'
+     />
+      
+    </Container>
+  );
+}
+
+
