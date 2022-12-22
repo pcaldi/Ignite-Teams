945e598b0f7ea3d8f256eaa03bae0e63630edabc
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-22 15:35:16 -0300

    ButtonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Icon } from "./styles";
+import { TouchableOpacityProps } from "react-native";
+
+
+type Props = TouchableOpacityProps & {
+
+}
+
+
+export function ButtonIcon({}: Props){
+  return (
+    <Container>
+      <Icon
+        name='house'
+        type='PRIMARY'
+      />
+    </Container>
+  );
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 
 export function Players(){
@@ -11,6 +12,7 @@ export function Players(){
         title="Nome da turma"
         subtitle="Adicione a galera e separe os times"
       />
+      <ButtonIcon/>
     </Container>
   );
 }
