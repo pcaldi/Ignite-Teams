19d8017951be26778278c4bef374d755a72fea02
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-21 21:27:42 -0300

    Component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export  function Groups() {
-  const [groups, setGroups] = useState(['Friends']);
+  const [groups, setGroups] = useState([]);
 
 
 
@@ -16,18 +17,24 @@ export  function Groups() {
     <Container>
      <Header />
      <Highlight
-      title='Turmas'
-      subtitle='Jogue com a sua turma'
+      title="Turmas"
+      subtitle="Jogue com a sua turma"
      />
 
      <FlatList
-      data={groups}
-      keyExtractor={item => item}
-      renderItem={({item}) => (
-        <GroupCard 
-        title={item} 
-        />
-      )}
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard 
+          title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1} }
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Cadastre a primeira turma!"
+          />
+        )}
      />
 
       
