848794966b608a9e81abd872e19fda5de8261428
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-23 16:51:16 -0300

    Finish Screen Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,6 +7,8 @@ import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
@@ -14,7 +16,7 @@ import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players(){
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Paulo', 'Loiro', 'Tico']);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -58,7 +60,18 @@ export function Players(){
             name={item}
             onRemove={() => {}}
             />
-          )}     
+          )}   
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Não há pessoas nesse time."
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[ { paddingBottom: 100}, players.length === 0 && { flex: 1} ]}
+      />
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
       />
     </Container>
   );
