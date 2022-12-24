2ffdba97da00e205d0c45d038ae18a30021baebe
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-23 21:44:03 -0300

    Passando parametros pelas rotas

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+import { useRoute } from "@react-navigation/native";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
@@ -13,16 +14,22 @@ import { Button } from "@components/Button";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 
+type  RouteParams =  {
+  group: string;
+}
 
 export function Players(){
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([]);
 
+  const route = useRoute();
+  const {group} = route.params as RouteParams;
+
   return (
     <Container>
       <Header showBackButton/>
       <Highlight
-        title="Nome da turma"
+        title={group}
         subtitle="Adicione a galera e separe os times"
       />
       <Form>
