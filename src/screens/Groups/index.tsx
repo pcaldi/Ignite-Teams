87cc52417574cd1562b4fd965fc26d4c68b68c05
Paulo Ricardo Caldi <pcaldi@gmail.com>
2022-12-25 20:04:42 -0300

    useFocusEffect

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -11,14 +13,25 @@ import { Button } from '@components/Button';
 import { Container } from './styles';
 
 export  function Groups() {
-  const [groups, setGroups] = useState([]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
   function handleNewGroup(){
     navigation.navigate('new')
   }
- 
+  async function fetchGroups(){
+    try {
+    const data =  await groupsGetAll();
+    setGroups(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useFocusEffect(useCallback(() =>{
+    fetchGroups();
+  },[]));
 
   return (
     <Container>
